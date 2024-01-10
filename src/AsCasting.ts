d0d5b2542854ecf748ea6c3ba32f9f1defd39e23
jxxu <jxxu@taifinance.cn>
2024-01-10 22:53:24 +0800

    chore(remove example file)

--- src/AsCasting.ts
+++
@@ -1,8 +0,0 @@
-{
-    type Todo = {
-        name: string;
-        completed: boolean;
-    };
-    
-    fetch('we').then((res) => res.json()).then((todo) => todo as Todo).then((todo) => todo);
-}
